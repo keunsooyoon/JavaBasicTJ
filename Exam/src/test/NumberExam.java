f21920c8ea5bcf75c677443f53b781dcef0df4d6
Austin <austiny@snu.ac.kr>
2021-12-10 16:57:32 +0900

    done

---
+++ Exam/src/test/NumberExam.java
@@ -0,0 +1,63 @@
+package test;
+
+public class NumberExam {
+    public static void main(String[] args) {
+    	
+//    	int i = 0;
+//    	System.out.println(i++);  //후증가
+//    	System.out.println(i);
+    	
+    	
+    	int i = 0;
+    	System.out.println(++i);  //증가
+    	System.out.println(i);   	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	// ++
+    	
+//    	int i = 0;
+//    	int j = 10;
+//    	
+//    	
+//    	i++;
+//    	j--;
+//    	
+//    	System.out.println(i);
+//    	System.out.println(j);
+//    	
+//    	
+//    	
+
+    	
+        // 숫자 자료형    
+
+        // 1. 정수  2, 100 ,300
+            //   int(***)
+
+            //   long (43214321L   숫자 끝에 대문자 L)
+
+        // 2. 실수  3.14
+                // double(***)
+
+                // float(3.14543253254543F 숫자 끝에 대문자F)
+
+//        int a = 10;
+//        int b = 5;
+//
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+    }
+}
